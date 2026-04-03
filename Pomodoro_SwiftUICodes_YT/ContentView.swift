46e603bd13bf6c8032274058d67a76482b7c7546
app-kaisha <app.kaihatsusha@gmail.com>
2026-04-03 13:57:06 +0100

    Initial Commit

---
+++ Pomodoro_SwiftUICodes_YT/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Pomodoro_SwiftUICodes_YT
+//
+//  Created by app-kaihatsusha on 03/04/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomodoro_SwiftUICodes_YT/Pomodoro_SwiftUICodes_YTApp.swift
@@ -0,0 +1,18 @@
+//
+//  Pomodoro_SwiftUICodes_YTApp.swift
+//  Pomodoro_SwiftUICodes_YT
+//
+//  Created by app-kaihatsusha on 03/04/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_SwiftUICodes_YTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
