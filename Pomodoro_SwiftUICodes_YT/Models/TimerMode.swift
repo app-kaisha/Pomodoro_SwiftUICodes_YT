d0c92610fa18d1e1368e77070111cac1fc537776
app-kaisha <app.kaihatsusha@gmail.com>
2026-04-03 14:02:38 +0100

    Initial commit - base setup

---
+++ Pomodoro_SwiftUICodes_YT/Models/TimerMode.swift
@@ -0,0 +1,9 @@
+//
+//  TimerMode.swift
+//  Pomodoro_SwiftUICodes_YT
+//
+//  Created by app-kaihatsusha on 03/04/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import Foundation
